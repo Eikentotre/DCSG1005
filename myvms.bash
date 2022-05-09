5a1daa1a0105ed0de2558fa7ab6523587d941697
Eiken <andesei@stud.ntnu.no>
2022-05-09 17:31:47 +0200

    script(myvms.ps1): oppdatert med iper

--- myvms.bash
+++ myvms.bash
@@ -4,16 +4,16 @@
 # usage: myvms.bash [mgr|cl1|dc1|srv1|mgra|cl1a|dc1a|srv1a]
 
 # floating ip's and the Admin-user-password for each host (retrieve from SkyHiGh):
-mgr_ip=
-mgr_pw=
-cl1_ip=
-cl1_pw=
-dc1_ip=
-dc1_pw=
-srv1_ip=
-srv1_pw=
+mgr_ip="10.212.142.194"
+mgr_pw="nmT4CAwSXD828wV0IdhU"
+cl1_ip="10.212.142.104"
+cl1_pw="BpJ1JkAN5Lnl8i8kyHX5"
+dc1_ip="10.212.141.254"
+dc1_pw="eYNhi0M5G82g94RHOBUa"
+srv1_ip="10.212.136.97"
+srv1_pw="EMkekEYisBk4rpsBm1oD"
 # domain administrator password (you set this yourself when creating the domain):
-dc1a_pw=
+dc1a_pw="xh.n!lBR+bZ]ryj>omN("
 
 logintype="$1"
 
